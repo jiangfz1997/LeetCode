80629b8a4ca255b5397376dc0512eee5d0a1517c
jiangfz1997 <63608649+jiangfz1997@users.noreply.github.com>
2020-04-13 15:22:16 -0400

    What reminds fixed: Differences between Hash HashSet and HashMap

--- src/Top100Liked/TwoSum_001.java
+++ src/Top100Liked/TwoSum_001.java
@@ -1,6 +1,31 @@
 package Top100Liked;
 
-public class TwoSum_001 {
+import java.util.HashMap;
 
+/*
+* Definitely the question I've done the most time.
+* How to fix: Use HashMap to record all the number
+* Complexity: O(n)
+* What's new: Learned How to use HashMap
+*
+* What reminds fixed: Differences between Hash HashSet and HashMap
+*
+* */
+public class TwoSum_001 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int result[] = {0,0};
+        for(int i = 0; i <= nums.length; i++){
+            if(map.get(target-nums[i]) != null){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
 
 }
