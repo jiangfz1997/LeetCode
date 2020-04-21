cc42c2f625648465f2fb3763fcc5c3f36e181d42
jiangfz1997 <63608649+jiangfz1997@users.noreply.github.com>
2020-04-20 23:43:35 -0400

    Non

---
+++ src/Array/PascalTriangleII.java
@@ -0,0 +1,42 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        // First base case; if user requests zero rows, they get zero rows.
+
+
+        // Second base case; first row is always [1].
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+        if (rowIndex == 0) {
+            return triangle.get(0);
+        }
+        for (int rowNum = 1; rowNum <= rowIndex; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum-1);
+
+            // The first row element is always 1.
+            row.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+
+            // The last row element is always 1.
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle.get(rowIndex);
+    }
+}
