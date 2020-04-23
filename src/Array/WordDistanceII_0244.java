2c0b8154ba3e9eec17ef5f253f4d232b698f4bbf
jiangfz1997 <63608649+jiangfz1997@users.noreply.github.com>
2020-04-22 23:42:13 -0400

    What's the difference between 243 and 244????

---
+++ src/Array/WordDistanceII_0244.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.*;
+
+public class WordDistanceII_0244 {
+    Map<String, ArrayList<Integer>> map;
+
+    public WordDistanceII_0244(String[] words) {
+        this.map = new HashMap<>();
+        for(int i = 0; i < words.length; i++) {
+            if(map.containsKey(words[i]))
+                map.get(words[i]).add(i);
+            else
+                map.put(words[i], new ArrayList<Integer>(Arrays.asList(i)));
+        }
+    }
+
+    public int shortest(String word1, String word2) {
+        if(word1 == null || word2 == null)
+            return Integer.MAX_VALUE;
+
+        List<Integer> word1s = map.get(word1);
+        List<Integer> word2s = map.get(word2);
+        int minDistance = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+
+        while(i < word1s.size() && j < word2s.size()) {
+            minDistance = Math.min(minDistance, Math.abs(word1s.get(i) - word2s.get(j)));
+            if(word1s.get(i) < word2s.get(j))
+                i++;
+            else
+                j++;
+        }
+
+        return minDistance;
+    }
+}
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance obj = new WordDistance(words);
+ * int param_1 = obj.shortest(word1,word2);
+ */
