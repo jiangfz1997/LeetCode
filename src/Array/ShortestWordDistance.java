15d992701dd78435d9ec1b3838a974bbe821e5ea
jiangfz1997 <63608649+jiangfz1997@users.noreply.github.com>
2020-04-22 23:23:36 -0400

    Non

---
+++ src/Array/ShortestWordDistance.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class ShortestWordDistance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int i1 = -1, i2 = -1;
+        int minDistance = words.length;
+        int currentDistance;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                i1 = i;
+            } else if (words[i].equals(word2)) {
+                i2 = i;
+            }
+
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+            }
+        }
+        return minDistance;
+    }
+
+    public static void main(String[] args){
+        ShortestWordDistance s = new ShortestWordDistance();
+        String[] sb = {"practice", "make", "perfect", "coding", "make"};
+        System.out.println(s.shortestDistance(sb, "coding", "practice"));
+    }
+
+}
