fb8d9495aeaf9c49b84037f358aa0fb9820cfba1
jiangfz1997 <63608649+jiangfz1997@users.noreply.github.com>
2020-05-31 21:36:18 -0400

    矩阵旋转的规律

---
+++ src/Array/RotateMatrix_0048.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class RotateMatrix_0048 {
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            for(int i = 0; i < n/2+n%2; i++){
+                for(int j = 0; j < n/2; j++){
+                    int[] tmp = new int[4];
+                    int row = i;
+                    int col = j;
+                    for(int k = 0; k < 4; k++){
+                        tmp[k] = matrix[row][col];
+                        int x = row;
+                        row = col;
+                        col = n-1-x;
+                    }
+
+                    for (int k = 0; k < 4; k++) {
+                        matrix[row][col] = tmp[(k + 3) % 4];
+                        int x = row;
+                        row = col;
+                        col = n - 1 - x;
+                    }
+                }
+            }
+        }
+    }
+}
